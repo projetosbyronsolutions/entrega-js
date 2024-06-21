6045d85d58ded8474b10700a2a6bdab867be7c4b
Pedro <pedrolopes@byronsolutions.com>
2024-06-21 11:17:45 -0300

    Adiciona a resposta 11

---
+++ src/pedro-henrique/solution-11.js
@@ -0,0 +1,15 @@
+function classificarTriangulo(n1, n2, n3){
+    if ((n1===n3) && (n1===n2)){
+        return"equilatero"
+    }
+    else
+    if ((n1===n3)|| (n1===n2)||(n2==n3)){
+        return "isosceles"
+    }
+    else{
+        return "escaleno"
+    }
+    
+}
+
+module.exports=classificarTriangulo
