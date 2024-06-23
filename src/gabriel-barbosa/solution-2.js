132574922ed8645154973448ce62bbe00e027197
Gabriel <gbfsap.gf@gmail.com>
2024-06-23 06:00:51 -0300

    Exercício 02 resolvido

---
+++ src/gabriel-barbosa/solution-2.js
@@ -0,0 +1,11 @@
+function qtdTruthy(lista){
+    let listaTrue = lista.filter((Element) => {
+        return Element
+    })
+
+    return listaTrue.length
+}
+
+list = [57, NaN, "Hello World", 0, [], "", undefined]
+
+console.log(qtdTruthy(list))
